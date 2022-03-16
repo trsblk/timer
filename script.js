3cf2363d60a0363ba8fb47c83c5b5758e2788ddd
trsblk <90855402+trsblk@users.noreply.github.com>
2022-03-16 23:30:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// Selecting elements
+const header = document.querySelector('header');
+const btnConfirm = document.querySelector('.btn-confirm');
+const mainSect = document.querySelector('main');
+const countEl = document.querySelector('.count');
+const btnTimerSet = document.querySelector('.timer-submit');
+//
+
+// Functions
+setTimeout(function () {
+  header.classList.remove('hidden');
+}, 2000);
+
+btnConfirm.addEventListener('click', function () {
+  // console.log("I got clicked");
+  mainSect.scrollIntoView({ behavior: 'smooth' });
+  mainSect.classList.remove('hidden');
+});
+
+let intervalTimer;
+
+btnTimerSet.addEventListener('click', function () {
+  const userInputSec = Number(document.querySelector('.input-number').value);
+
+  // Calculating remaining seconds
+  let sec = userInputSec % 60;
+
+  // Calculating remaining minutes
+  let min = Math.trunc(userInputSec / 60);
+  // Check if it is a running timer
+  if (intervalTimer) clearInterval(intervalTimer);
+
+  // Only positive seconds
+  if (userInputSec > 0) {
+    // Create timer
+
+    intervalTimer = setInterval(function () {
+      countEl.textContent = `${min.toString().padStart(2, '0')}:${sec
+        .toString()
+        .padStart(2, '0')}`;
+
+      // updating min and seconds
+      if (sec === 0 && min !== 0) {
+        sec = 60;
+        min--;
+      }
+
+      // if 00:00 then stop the timer
+      if (sec === 0 && min === 0) {
+        clearInterval(intervalTimer);
+      }
+      sec--;
+    }, 1000);
+  } else {
+    countEl.textContent = `00:00`;
+  }
+});
